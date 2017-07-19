2d483263454a55b9877f9122fd7af9b85bdc8bed
Fred Scott <fscott@southeast.edu>
2017-07-19 15:57:17 -0500

    Added starter index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<?php
+//1525 Program 1
+
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Change this to your name, not "your name" but your name right?</title>
+    </head>
+    <body>
+
+    </body>
+</html>
