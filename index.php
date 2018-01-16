cd2806b82ff5882a03fc02b02e6529600f7525d0
lw244130 <lw244130@LR00908.southeast.edu>
2018-01-16 14:08:19 -0600

    changed title and added comments

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <?php
 //1525 Program 1
+
+//Create a PHP script that generates and displays a random math question and a form that allows the user to answer the question. Once the user has submitted the answer, tell the user the answer and whether they were correct or not.
+//Error-trap the user entry to make sure the answer field is filled in. If not, respond back with the question/form again, including an error message on the page.
+//The program doesn't need to repeat and ask multiple questions. That will be added in later.
+//Use an external css file and apply some reasonable styling. Make it look like a real website and not just plain left aligned black text on a white background. I use the "I know it when I see it" standard.
+//Hint: Store the answer and/or the problem in the form as hidden inputs. This will enable your script to "remember" the original problem when the user submits the answer.
     $var = 'changing the original repo?';
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Change this to your name, not "your name" but your name right?</title>
+        <title>Loren Wetzel</title>
     </head>
     <body>
 
