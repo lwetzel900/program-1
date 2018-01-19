c5cd9a73b0eb0f7e25a0c3d9b00f1798151f6dd9
lwetz <lwetz@mydesktop.Home>
2018-01-18 23:37:51 -0600

    added css comments and cleaned up code

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
 <!DOCTYPE html>
 <?php
 //1525 Program 1
-//Create a PHP script that generates and displays a random math 
-//  question and a form that allows the user to answer the 
-//      question. Once the user has submitted the answer, 
-//          tell the user the answer and whether they were correct or not.
-//Error-trap the user entry to make sure the answer field is filled in. 
-//  If not, respond back with the question/form again, 
-//      including an error message on the page.
-//The program doesn't need to repeat and ask multiple questions. 
-//  That will be added in later.
-//Use an external css file and apply some reasonable styling. 
-//  Make it look like a real website and not just plain 
-//      left aligned black text on a white background. 
-//          I use the "I know it when I see it" standard.
-//Hint: Store the answer and/or the problem in the form as hidden inputs. 
-//  This will enable your script to "remember" the original 
-//      problem when the user submits the answer.
-
+//
+//I approched this the wrong way about. I misread the instructions and still feel I didn't do something right.
+//    It works, just not the way I exactly wanted it to. 
+//    After you get the right anser you can't refresh and start over.
+//I feel there is to many variables here. You may have wanted it simplier.
 $operands = array('+', '-', '*', '%');
 shuffle($operands);
 $ranOperand = $operands[0];
@@ -25,36 +13,44 @@ $var = rand(1, 10);
 $var2 = rand(1, 10);
 $question = "$var $ranOperand $var2";
 $answer = eval("return $question;");
-var_dump($question);
-var_dump($answer);
+//var_dump($question);
+//var_dump($answer);
 ?>
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Loren Wetzel</title>
+        <link rel="stylesheet" type="text/css" href="main.css" >
     </head>
     <body>
-        <?php if (!empty($error_message)) { ?>
-        <p><?php echo htmlspecialchars($error_message); 
-        $question = filter_input(INPUT_GET, 'questionHide');
-        $answer = filter_input(INPUT_GET, 'answerHide', FILTER_VALIDATE_INT);?></p>
-    <?php } ?>
+        <main>
+            <?php if (!empty($error_message)) { ?>
+                <p class="error"><?php
+                echo htmlspecialchars($error_message);
+            //the next two lines are how I got the same question and anser to be passed back from target.php
+                $question = filter_input(INPUT_GET, 'questionHide');
+                $answer = filter_input(INPUT_GET, 'answerHide', FILTER_VALIDATE_INT);
+                ?></p> <?php } ?>
 
-        <form action="target.php" method="get">
+            <form action="target.php" method="get">
 
-            <h1>Answer the question below.</h1>
-            <label><?php echo $question; ?> = </label>
-            <input type="text" name="answer"><br>
+                <h1>Answer the question</h1>
+                
+                <label><?php echo $question; ?> = </label>
+                <input type="text" name="answer"><br>
+                
+<!--                store answer-->
+                <input type="hidden" name="answerHide"
+                       value="<?php echo $answer; ?>">
+                
+<!--                store question-->
+                <input type="hidden" name="questionHide"
+                       value="<?php echo $question; ?>">
 
-            <input type="hidden" name="answerHide"
-                   value="<?php echo $answer; ?>">
+                <input type="submit" value="Check"><br>
 
-            <input type="hidden" name="questionHide"
-                   value="<?php echo $question; ?>">
-
-            <label>&nbsp;</label>
-            <input type="submit" value="Send"><br>
-
-        </form>
+            </form>
+        </main>
     </body>
 </html>
